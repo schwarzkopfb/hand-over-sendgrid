44860749103a41d147ad43139844363ce80bd80c
Balázs Schwarzkopf <schwarzkopfb@icloud.com>
2016-08-12 17:36:17 +0200

    implement core functionality

--- index.js
+++ index.js
@@ -3,12 +3,13 @@
 module.exports = HandoverSendgrid
 
 var inherits = require('util').inherits,
+    sendgrid = require('sendgrid'),
+    helper   = sendgrid.mail,
     Plugin   = require('hand-over/plugin')
 
 function HandoverSendgrid(opts) {
     Plugin.call(this, opts)
-
-    // todo
+    this.instance = sendgrid(opts)
 }
 
 inherits(HandoverSendgrid, Plugin)
@@ -16,7 +17,26 @@ inherits(HandoverSendgrid, Plugin)
 HandoverSendgrid.prototype.name = 'sendgrid'
 
 HandoverSendgrid.prototype.send = function (target, data, callback) {
+    var from    = new helper.Email(data.from),
+        to      = new helper.Email(data.to),
+        type    = data.contentType || data.content_type || 'text/html',
+        content = new helper.Content(type, data.content || data.body),
+        mail    = new helper.Mail(from, data.subject || '', to, content)
 
+  var request = this.instance.emptyRequest({
+    method: 'POST',
+    path:   '/v3/mail/send',
+    body:   mail.toJSON()
+  })
+
+  this.instance.API(request, function(err, res) {
+    // todo: validate response
+    // console.log(response.statusCode)
+    // console.log(response.body)
+    // console.log(response.headers)
+
+    callback()
+  })
 }
 
 HandoverSendgrid.prototype.unref = function () {
